423f0136695e25cb96561646cb76dc63d5d0d186
ADeleola <113690717+ADeleola@users.noreply.github.com>
2022-10-17 04:38:39 +0100

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,25 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 
-
-
 /**
- *
- *  * main - Entry point
- *
- *   * Program to write a few words
- *
- *    * with a new line
- *
- *     * Return: Always 0 (Success)
- *
- *      */
-
+ * main - Entry point
+ * Return: Always 1 (Success)
+ */
 int main(void)
-
 {
-
-		write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-
-			return (1);
-
+	write(STDOUT_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
