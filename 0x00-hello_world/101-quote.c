21d1692374ce0b464efc18d36b4a484eba3c333a
Abayomi Deleola <deleola40@gmail.com>
2022-10-19 17:36:31 -0700

    update

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,65 +1,29 @@
 #include <stdio.h>
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 
-#include <stdlib.h>
-
-=======
-#include <stdlib.h>
->>>>>>> a5e73fc5278a050c96fa828c9c9e25d1a7f5f27f
 #include <unistd.h>
 
-/**
-<<<<<<< HEAD
- *
- *  * main - Entry point
- *
- *   * Return: Always 1 (Success)
- *
- *    */
-
-=======
- * main - Entry point
- * Return: Always 1 (Success)
- */
->>>>>>> a5e73fc5278a050c96fa828c9c9e25d1a7f5f27f
-int main(void)
-{
-<<<<<<< HEAD
-
-		write(STDOUT_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-
-			return (1);
-
-=======
-	write(STDOUT_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
->>>>>>> a5e73fc5278a050c96fa828c9c9e25d1a7f5f27f
-=======
 #include <stdlib.h>
-#include <unistd.h>
+
+
 
 /**
- * main - Entry point
- * Return: Always 1 (Success)
- */
-int main(void)
-{
-	write(STDOUT_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
->>>>>>> a5e73fc5278a050c96fa828c9c9e25d1a7f5f27f
-=======
-#include <stdlib.h>
-#include <unistd.h>
+ *
+ *  * main - UNIX
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
 
-/**
- * main - Entry point
- * Return: Always 1 (Success)
- */
+
+
 int main(void)
+
 {
-	write(STDOUT_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+
 	return (1);
->>>>>>> a5e73fc5278a050c96fa828c9c9e25d1a7f5f27f
+
 }
