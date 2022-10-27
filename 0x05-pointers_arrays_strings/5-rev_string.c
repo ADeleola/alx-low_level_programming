9a1ad39e601a9da67c0a6a58b2cc37ed9e393203
Abayomi Deleola <deleola40@gmail.com>
2022-10-27 01:27:34 -0700

    pointer

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+
+/**
+ * rev_string - reverses a string.
+ * @s: input character
+ * Return: no
+ */
+
+
+void rev_string(char *s)
+
+{
+
+	int count = 0;
+
+	int i;
+
+	int j;
+
+
+
+	while (s[count] != '\0')
+
+	{
+
+										count++;
+	}
+	for (j = 0; j <= count; j++)
+
+	{
+											_putchar(s[j]);
+	}
+	_putchar('\n');
+
+	for (i = 1; i <= count; i++)
+										{
+
+										_putchar(s[count - i]);
+										}
+	_putchar('\n');
+
+}
